3a2541b9ddec82de7ad7a93f2d625e666acf48ff
Joshua Davian <joshuadavian@gmail.com>
2022-08-12 12:24:53 +0700

    api history search

---
+++ history/history_search.php
@@ -0,0 +1,30 @@
+<?php
+include '../connection.php';
+
+$user_id = $_POST['user_id'];
+$date = date('Y-m-d', strtotime($_POST['date']));
+
+$sql = "SELECT id, date, total, type FROM histories WHERE user_id = '$user_id' AND date = '$date' ORDER BY date DESC";
+$result = $connect->query($sql);
+
+if ($result->num_rows > 0) {
+    $data = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode([
+        'success' => true,
+        'message' => 'Data retrieved successfully.',
+        'data' => $data
+    ], JSON_PRETTY_PRINT);
+} else {
+    header('Content-Type: application/json');
+    http_response_code(404);
+    echo json_encode([
+        'success' => false,
+        'message' => 'Data not found.'
+    ], JSON_PRETTY_PRINT);
+}
